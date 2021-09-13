b826a042c93da2ca3b8722c5f8ce0f16e2dcc93c
Catherine Trevor <catherinetrevor@outlook.com>
2021-09-13 10:00:31 +0000

    bugfix: resolve webhook errors

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -6,12 +6,29 @@ class StripeWH_Handler:
 
     def __init__(self, request):
         self.request = request
-    
+
     def handle_event(self, event):
         """
         Handle a generic/unkown/unexpected webhook event
         """
         return HttpResponse(
+            content=f'Unhandled webhook received: {event["type"]}',
+            status=200)
+
+    def handle_payment_intent_succeeded(self, event):
+        """
+        Handle the payment_intent.succeeded webhook event
+        """
+        intent = event.data.object
+        print(intent)
+        return HttpResponse(
             content=f'Webhook received: {event["type"]}',
             status=200)
-            
+
+    def handle_payment_intent_payment_failed(self, event):
+        """
+        Handle the payment_intent.failed webhook event
+        """
+        return HttpResponse(
+            content=f'Webhook received: {event["type"]}',
+            status=200)
